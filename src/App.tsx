3213a8fc3a89b04ab7a930b395205714d389f127
Jed Borseth <jedborseth@desktop-popos.va.shawcable.net>
2022-09-19 16:31:56 -0700

    create template

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen grid place-items-center bg-neutral-800">
+      <h1 className="text-white text-2xl">Jed Borseth React Template</h1>
+    </div>
+  );
+}
+
+export default App;
